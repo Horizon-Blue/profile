6335ca93cdf17203bdc51886d93a4823a0a1f56e
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-11-03 19:51:36 -0500

    Initialize React and antd

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+
+const { Header, Sider, Content, Footer } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header>header</Header>
+        <Layout>
+          <Sider>left sidebar</Sider>
+          <Content>main content</Content>
+          <Sider>right sidebar</Sider>
+        </Layout>
+        <Footer>footer</Footer>
+      </Layout>
+    );
+  }
+}
+
+export default App;
