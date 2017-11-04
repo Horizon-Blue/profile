e2d324e9eff4d83dd1f863deca19a1f32c54f1af
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-11-04 17:53:29 -0500

    Add under construction spin

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,24 @@
 import React, { PureComponent } from 'react';
+import { Spin, Row, Col } from 'antd';
 import Bio from '@Bio';
 
 class Main extends PureComponent {
     render = () => {
-        return <div className="main-content"><Bio /></div>;
+        return (
+            <div className="main-content">
+                <Col xs={24} sm={18}>
+                    <Row>
+                        <Bio />
+                    </Row>
+                    <hr />
+                    <Row>
+                        <Col span={4} push={10} style={{ marginTop: 10 }}>
+                            <Spin size="large" tip="Under Construction..." />
+                        </Col>
+                    </Row>
+                </Col>
+            </div>
+        );
     };
 }
 
